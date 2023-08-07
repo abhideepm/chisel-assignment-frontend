6360368e37a1fc47e3be8a24a60f5158b2a2741e
Abhideep Maity <abhideepm@gmail.com>
2023-08-07 22:26:37 +0530

    basic tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Tab } from "semantic-ui-react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container>
+      <h1>Todo App</h1>
+      <h3>Boards</h3>
+      <Tab
+        panes={[
+          {
+            menuItem: "Tab 1",
+            render: () => <Tab.Pane>Tab 1 Content</Tab.Pane>
+          },
+          {
+            menuItem: "Tab 2",
+            render: () => <Tab.Pane>Tab 2 Content</Tab.Pane>
+          },
+          {
+            menuItem: "Tab 3",
+            render: () => <Tab.Pane>Tab 3 Content</Tab.Pane>
+          }
+        ]}
+      />
+    </Container>
+  );
+};
 
-export default App
+export default App;
